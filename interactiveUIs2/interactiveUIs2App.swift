2425f890d412872cd02255a603bf1e107420c76c
Angela <angelatruong705@gmail.com>
2023-07-27 13:01:01 -0400

    Initial Commit

---
+++ interactiveUIs2/interactiveUIs2App.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIs2App.swift
+//  interactiveUIs2
+//
+//  Created by Scholar on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIs2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
